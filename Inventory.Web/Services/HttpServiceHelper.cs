9a9e757b0312de9b8785283640500937daf81e09
gezequiel <g.ezequiel@outlook.com>
2016-01-04 21:54:35 -0300

    Add initial service helper

---
+++ Inventory.Web/Services/HttpServiceHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory.Web.Services
+{
+    public class HttpServiceHelper
+    {
+        public TResult Post<TConvertType, TResult>(TConvertType item, string uriPart, string uriBase)
+        {
+            return default(TResult);
+        }
+    }
+}
